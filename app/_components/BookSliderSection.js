d666e10a0df18b822ec34703e161868b5661d8ac
AmirFarahani82 <amyrfrahany427@gmail.com>
2025-12-28 15:53:31 +0330

    Fix React Server Components CVE vulnerabilities

--- app/_components/BookSliderSection.js
+++ app/_components/BookSliderSection.js
@@ -3,7 +3,7 @@ import BookSlider from "./BookSlider";
 
 async function BookSliderSection() {
   const { data } = await getbooks({});
-
+  console.log(data);
   return (
     <section className="relative h-[50vh] w-full overflow-hidden sm:h-screen">
       <BookSlider books={data} />
